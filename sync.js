da0adcb4a6546efa8883fec201420d5a826f0204
abdelhay <muhammad.abdelhay@vodafone.com>
2023-03-15 23:48:17 +0200

    create target db if not exist

--- sync.js
+++ sync.js
@@ -15,22 +15,35 @@ const targetConnection = mysql.createConnection({
   host: process.env.tgt_host,
   user: process.env.tgt_user,
   password: process.env.tgt_password,
-  database: tgt_db,
 });
 
-sourceConnection.query(`show tables;`, (err, results) => {
+targetConnection.query("CREATE DATABASE IF NOT EXISTS `" + tgt_db + "`", (err, result) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    let tables = [];
-    Object.entries(results).forEach((obj) => {
-      tables.push(Object.values(obj[1])[0]);
+    sourceConnection.query(`show tables;`, (err, results) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let tables = [];
+        Object.entries(results).forEach((obj) => {
+          tables.push(Object.values(obj[1])[0]);
+        });
+        tables.forEach((table) => {
+          targetConnection.query(
+            "CREATE TABLE IF NOT EXISTS `" +
+              tgt_db +
+              "`.`" +
+              table +
+              "` AS SELECT * FROM `" +
+              src_db +
+              "`.`" +
+              table +
+              "`"
+          );
+        });
+        console.log("sync is complete");
+      }
     });
-    tables.forEach((table) => {
-      targetConnection.query(
-        "CREATE TABLE IF NOT EXISTS `" + tgt_db + "`.`" + table + "` AS SELECT * FROM `" + src_db + "`.`" + table + "`"
-      );
-    });
-    console.log("sync is complete");
   }
 });
