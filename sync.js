ecab4abc19fd2a45c0ae3988ca40e51bf286698e
unknown <m.abdelhay2015@gmail.com>
2023-03-22 01:54:12 +0200

    check if the two tables have the same data before sync

--- sync.js
+++ sync.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 const child_process = require("child_process");
+const _ = require("lodash");
+const util = require("util");
 
 console.time("syncing");
 
@@ -73,48 +75,32 @@ targetConnection.query("CREATE DATABASE IF NOT EXISTS `" + tgt_db + "`", (err, r
               console.log(error);
             }
 
-            checkSumAndSync(table);
+            sourceConnection.query(`SELECT * FROM ${table}`, (error, src_results) => {
+              if (error) throw error;
 
-            // child_process.exec(
-            //   `perl pt-table-sync-local.pl --execute --no-check-slave --verbose --no-unique-checks h=${process.env.src_host},P=${process.env.src_port},u=${process.env.src_user},p=${process.env.src_password},D=${process.env.src_db},t=${table} h=${process.env.tgt_host},P=${process.env.tgt_port},u=${process.env.tgt_user},p=${process.env.tgt_password},D=${process.env.tgt_db}`,
-            //   (err, stdout, stderr) => {
-            //     if (err) {
-            //       console.error(stderr);
-            //     } else {
-            //       console.log(stdout);
-            //       console.timeLog("syncing");
-            //     }
-            //   }
-            // );
+              newTargetConnection.query(`SELECT * FROM ${table}`, (error, tgt_results) => {
+                if (error) throw error;
+
+                if (_.isEqual(src_results, tgt_results)) {
+                  console.log("The tables have the same data");
+                } else {
+                  child_process.exec(
+                    `perl pt-table-sync-local.pl --execute --no-check-slave --verbose --no-unique-checks h=${process.env.src_host},P=${process.env.src_port},u=${process.env.src_user},p=${process.env.src_password},D=${process.env.src_db},t=${table} h=${process.env.tgt_host},P=${process.env.tgt_port},u=${process.env.tgt_user},p=${process.env.tgt_password},D=${process.env.tgt_db}`,
+                    (err, stdout, stderr) => {
+                      if (err) {
+                        console.error(stderr);
+                      } else {
+                        console.log(stdout);
+                        console.timeLog("syncing");
+                      }
+                    }
+                  );
+                }
+              });
+            });
           });
         });
       });
     }
   });
 });
-
-function checkSumAndSync(table) {
-  // Run pt-table-checksum command
-  const checksumCommand = `perl pt-table-checksum-local.pl --host=${process.env.src_host} --user=${process.env.src_user} --password=${process.env.src_password} --databases ${process.env.src_db} --tables ${table} --replicate ${process.env.src_db}.checksums --chunk-size=1000 --create-replicate-table --empty-replicate-table --no-check-binlog-format`;
-
-  child_process.exec(checksumCommand, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error running pt-table-checksum: ${err}`);
-      return;
-    }
-
-    console.log(`pt-table-checksum output: ${stdout}`);
-
-    // Run pt-table-sync command
-    const syncCommand = `perl pt-table-sync-local.pl --host=${process.env.tgt_host} --user=${process.env.tgt_user} --password=${process.env.tgt_password} --replicate ${process.env.tgt_db}.checksums --sync-to-master h=${process.env.src_host},P=${process.env.src_port},u=${process.env.src_user},p=${process.env.src_password},D=${process.env.src_db},t=${table} h=${process.env.tgt_host},P=${process.env.tgt_port},u=${process.env.tgt_user},p=${process.env.tgt_password},D=${process.env.tgt_db},t=${table}`;
-
-    child_process.exec(syncCommand, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error running pt-table-sync: ${err}`);
-        return;
-      }
-
-      console.log(`pt-table-sync output: ${stdout}`);
-    });
-  });
-}
